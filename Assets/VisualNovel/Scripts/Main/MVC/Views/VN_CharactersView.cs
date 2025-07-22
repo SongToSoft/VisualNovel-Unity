75473b3fd047652108b8f4b0509b002b80b24cb3
dmitriy.bogdanov <bogdanovdi888@gmail.com>
2025-07-23 02:03:26 +1000

    Add character animation
    Add character animation

--- Assets/VisualNovel/Scripts/Main/MVC/Views/VN_CharactersView.cs
+++ Assets/VisualNovel/Scripts/Main/MVC/Views/VN_CharactersView.cs
@@ -5,8 +5,16 @@ using UnityEngine.TextCore.Text;
 
 public class VN_CharactersView : MonoBehaviour
 {
+    [SerializeField] private Image m_characterMiddleImage;
     [SerializeField] private Image m_characterRightImage;
+    [SerializeField] private bool m_fadeEnabled = true;
+    [SerializeField] private float m_animationDuration = 2f;
+    [SerializeField] private float m_movementDistance = 1f;
+    [SerializeField] private float m_fadeSpeed = 0.05f;
 
+    private Vector3 m_defaultCharacterPosition;
+
+    private bool m_isCharacterReverse;
     private string m_characterName = "";
 
     public void Start()
@@ -14,6 +22,7 @@ public class VN_CharactersView : MonoBehaviour
         if (m_characterRightImage)
         {
             m_characterRightImage.color = new Color(255, 255, 255, 255);
+            m_defaultCharacterPosition = m_characterRightImage.transform.position;
         }
     }
 
@@ -33,16 +42,58 @@ public class VN_CharactersView : MonoBehaviour
         {
             return;
         }
-        if (m_characterName == "" || m_characterName != character.path)
+        if (m_characterName == "" || !m_fadeEnabled)
         {
             m_characterName = character.path;
+            m_isCharacterReverse = character.reverse;
             characterImage.sprite = Resources.Load<Sprite>(m_characterName);
             characterImage.transform.localScale = new Vector3(character.reverse ? -1 : 1, 1, 1);
+        }
+        else
+        {
+            if (m_characterName != character.path)
+            {
+                m_characterName = character.path;
+                m_isCharacterReverse = character.reverse;
+                StartCoroutine(AnimateImage());
+            }
         }
     }
 
     public void SetCharacterVisible(bool visible)
     {
         m_characterRightImage.gameObject.SetActive(visible);
+    }
+
+    private IEnumerator AnimateImage()
+    {
+        //TODO: Fix faster click on next
+        float timer = 0f;
+
+        m_characterRightImage.color = new Color(m_characterRightImage.color.r, m_characterRightImage.color.g, m_characterRightImage.color.b, 1);
+        m_characterRightImage.transform.position = m_defaultCharacterPosition;
+
+        while (timer < m_animationDuration / 2)
+        {
+            timer += Time.deltaTime;
+            m_characterRightImage.rectTransform.position += (m_movementDistance / (m_animationDuration / 2)) * Time.deltaTime * Vector3.right;
+            m_characterRightImage.color -= new Color(0, 0, 0, m_fadeSpeed);
+            yield return null;
+        }
+
+        m_characterRightImage.transform.localScale = new Vector3(m_isCharacterReverse ? -1 : 1, 1, 1);
+        m_characterRightImage.color = new Color(m_characterRightImage.color.r, m_characterRightImage.color.g, m_characterRightImage.color.b, 0);
+        m_characterRightImage.sprite = Resources.Load<Sprite>(m_characterName);
+
+        while (timer < m_animationDuration)
+        {
+            timer += Time.deltaTime;
+            m_characterRightImage.rectTransform.position -= (m_movementDistance / (m_animationDuration / 2)) * Time.deltaTime * Vector3.right;
+            m_characterRightImage.color += new Color(0, 0, 0, m_fadeSpeed);
+            yield return null;
+        }
+
+        m_characterRightImage.rectTransform.position = m_defaultCharacterPosition;
+        m_characterRightImage.color = new Color(m_characterRightImage.color.r, m_characterRightImage.color.g, m_characterRightImage.color.b, 1);
     }
 }
